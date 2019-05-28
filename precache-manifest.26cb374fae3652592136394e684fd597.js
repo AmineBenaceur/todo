b173e79014056a1530529d712b7b1294c30e455f
Amine <abenaceur12@gmail.com>
2019-05-28 17:35:30 -0600

    Updates

---
+++ precache-manifest.26cb374fae3652592136394e684fd597.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf10cc09dbedd52b89cda323e12469b",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "4ca9e957416a4a8e2cb5",
+    "url": "/todo/static/css/main.5bb6ea30.chunk.css"
+  },
+  {
+    "revision": "ac023df88135a8254d05",
+    "url": "/todo/static/js/2.3580c089.chunk.js"
+  },
+  {
+    "revision": "4ca9e957416a4a8e2cb5",
+    "url": "/todo/static/js/main.7466a6ab.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "aminebenaceur.github.io/todo/precache-manifest.3d240be0fc10c81dfbaab492e06ab01d.js"
+  "/todo/precache-manifest.26cb374fae3652592136394e684fd597.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("aminebenaceur.github.io/todo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
