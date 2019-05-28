c47f7c4b9c671290277eb1e645bf306cdfeda1d6
Amine <abenaceur12@gmail.com>
2019-05-28 17:19:50 -0600

    Updates

---
+++ precache-manifest.3d240be0fc10c81dfbaab492e06ab01d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3f2e414c21cf0b49a3c12a4e53563f",
+    "url": "aminebenaceur.github.io/todo/index.html"
+  },
+  {
+    "revision": "e904e77b78d496d97947",
+    "url": "aminebenaceur.github.io/todo/static/css/main.5bb6ea30.chunk.css"
+  },
+  {
+    "revision": "ac023df88135a8254d05",
+    "url": "aminebenaceur.github.io/todo/static/js/2.3580c089.chunk.js"
+  },
+  {
+    "revision": "e904e77b78d496d97947",
+    "url": "aminebenaceur.github.io/todo/static/js/main.f0c2889c.chunk.js"
+  },
+  {
+    "revision": "1b72bb94260f5a72c5fe",
+    "url": "aminebenaceur.github.io/todo/static/js/runtime~main.e219ef85.js"
+  }
+]);
